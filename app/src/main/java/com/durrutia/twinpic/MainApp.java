a30a47235190d11bd6d0edb4954b0af9f8cdf174
Diego Urrutia Astorga <durrutia@ucn.cl>
2016-11-02 20:34:40 -0300

    Updated: Cleaned code.

--- app/src/main/java/com/durrutia/twinpic/MainApp.java
+++ app/src/main/java/com/durrutia/twinpic/MainApp.java
@@ -1,28 +1,30 @@
 package com.durrutia.twinpic;
 
 import android.app.Application;
-import android.os.SystemClock;
 
-import com.durrutia.twinpic.domain.Pic;
 import com.durrutia.twinpic.util.DeviceUtils;
-import com.durrutia.twinpic.util.PackageUtils;
+import com.google.common.base.Stopwatch;
 import com.squareup.leakcanary.LeakCanary;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Created by durrutia on 20-Oct-16.
+ * Singleton principal de la aplicacion.
+ *
+ * @author Diego P. Urrutia Astorga
+ * @version 20161020
  */
 @Slf4j
 public class MainApp extends Application {
 
     /**
-     *
+     * Al iniciarse por primera vez la aplicacion.
      */
     @Override
     public void onCreate() {
         super.onCreate();
 
+        // LeakCanary solo en aplicacion principal
         if (LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
             // You should not init your app in this process.
@@ -30,36 +32,14 @@ public class MainApp extends Application {
         }
 
         // Startdate
-        long startDate = SystemClock.elapsedRealtime();
+        final Stopwatch stopwatch = Stopwatch.createStarted();
 
-        log.debug("Install LeakCanary ..");
+        log.debug("Install LeakCanary in {}.", DeviceUtils.getDeviceName(this));
 
         LeakCanary.install(this);
-        // Normal app init code...
 
-        final String deviceName = DeviceUtils.getDeviceName(this);
-        log.debug("DeviceName: {}", deviceName);
-
-        final String deviceId = DeviceUtils.getDeviceId(this);
-        log.debug("DeviceId: {}", deviceId);
-        log.debug("UserId: {}", getUserAgent());
-
-        log.debug("Building ..");
-
-        final Pic pic = Pic.builder()
-                .id(-1L)
-                .deviceId(deviceId)
-                .date(System.currentTimeMillis())
-                .build();
-
-        log.debug("Pic: {}", pic);
-
-        long endDate = SystemClock.elapsedRealtime() - startDate;
-        log.info("Init in {}ms.", endDate);
+        log.info("Init in {}.", stopwatch);
 
     }
 
-    public String getUserAgent() {
-        return "tp-android/" + PackageUtils.getVersionName(this);
-    }
 }
