7160c393c16498ff62509ecc959008d09e1a5c21
Diego Urrutia Astorga <durrutia@ucn.cl>
2016-11-02 20:21:58 -0300

    Refactor: Division de la misma prueba (android + desktop).

--- app/src/androidTest/java/com/durrutia/twinpic/domain/TestModelAndroid.java
+++ app/src/androidTest/java/com/durrutia/twinpic/domain/TestModelAndroid.java
@@ -2,22 +2,12 @@ package com.durrutia.twinpic.domain;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.core.deps.guava.base.Stopwatch;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.durrutia.twinpic.util.DeviceUtils;
-import com.raizlabs.android.dbflow.config.FlowConfig;
-import com.raizlabs.android.dbflow.config.FlowManager;
-import com.raizlabs.android.dbflow.sql.language.SQLite;
-
-import org.apache.commons.lang3.RandomUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
-
-import java.util.Date;
-import java.util.List;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -40,58 +30,11 @@ public class TestModelAndroid {
      */
     @Test
     public void testDatabase() {
+
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        // Cronometro
-        final Stopwatch stopwatch = Stopwatch.createStarted();
-
-        log.debug("Testing database ..");
-
-        // Inicializacion
-        FlowManager.init(new FlowConfig.Builder(appContext)
-                .openDatabasesOnInit(true)
-                .build());
-
-        log.debug("DB initialized in: {}.", stopwatch);
-
-        stopwatch.reset().start();
-
-        // Ciclo para insertar 100 objetos en la bd
-        for (int i = 1; i <= 100; i++) {
-
-            stopwatch.reset().start();
-
-            final Pic pic = Pic.builder()
-                    .deviceId(DeviceUtils.getDeviceId(appContext))
-                    .latitude(RandomUtils.nextDouble())
-                    .longitude(RandomUtils.nextDouble())
-                    .date(new Date().getTime())
-                    .positive(RandomUtils.nextInt(0, 100))
-                    .negative(RandomUtils.nextInt(0, 100))
-                    .warning(RandomUtils.nextInt(0, 2))
-                    .build();
-
-            // Commit
-            pic.save();
-
-            log.debug("Saved {} in {}.", i, stopwatch);
-        }
-
-        stopwatch.reset().start();
-
-        List<Pic> pics = SQLite.select().from(Pic.class).queryList();
-        log.debug("Result: {} in {}.", pics.size(), stopwatch);
-
-        stopwatch.reset().start();
-
-        for (final Pic p : pics) {
-            log.debug("{}", p);
-        }
-
-        FlowManager.destroy();
-
-        log.debug("Finished in {}.", stopwatch);
+        com.durrutia.twinpic.Test.testDatabase(appContext);
 
     }
 
---
+++ app/src/test/java/com/durrutia/twinpic/domain/TestModelDesktop.java
@@ -0,0 +1,43 @@
+package com.durrutia.twinpic.domain;
+
+import android.provider.Settings;
+
+import com.durrutia.twinpic.BaseTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.robolectric.RuntimeEnvironment;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Diego P. Urrutia Astorga
+ * @version 20161102
+ */
+@Slf4j
+public class TestModelDesktop extends BaseTest {
+
+    /**
+     * Timming
+     */
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(10);
+
+    /**
+     * Testing
+     */
+    @Test
+    public void testDatabase() {
+
+        Settings.Secure.putString(RuntimeEnvironment.application.getContentResolver(),
+                Settings.Secure.ANDROID_ID,
+                "D3v1c31d"
+        );
+
+        com.durrutia.twinpic.Test.testDatabase(RuntimeEnvironment.application);
+
+    }
+
+
+}
