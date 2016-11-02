46474e036343c0956ae62d5428cc81d10d3ce9ac
Diego Urrutia Astorga <durrutia@ucn.cl>
2016-11-02 20:21:28 -0300

    Added: Clase Twin que une dos Pic.

---
+++ app/src/main/java/com/durrutia/twinpic/domain/Twin.java
@@ -0,0 +1,48 @@
+package com.durrutia.twinpic.domain;
+
+import com.durrutia.twinpic.Database;
+import com.raizlabs.android.dbflow.annotation.ForeignKey;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by durrutia on 02-Nov-16.
+ */
+@Slf4j
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(
+        database = Database.class,
+        cachingEnabled = false,
+        orderedCursorLookUp = true, // https://github.com/Raizlabs/DBFlow/blob/develop/usage2/Retrieval.md#faster-retrieval
+        cacheSize = Database.CACHE_SIZE
+)
+public class Twin extends BaseModel {
+
+    /**
+     * Pic local
+     */
+    @Getter
+    @Setter
+    @PrimaryKey
+    @ForeignKey(tableClass = Pic.class)
+    Pic local;
+
+    /**
+     * Pic desde el servidor
+     */
+    @Getter
+    @Setter
+    @PrimaryKey
+    @ForeignKey(tableClass = Pic.class)
+    Pic remote;
+
+}
